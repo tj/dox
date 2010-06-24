6e7e6539567d051701bbee45a6863b23c78c5452
Tj Holowaychuk <tj@vision-media.ca>
2010-06-23 20:42:22 -0700

    Added example files

--- examples/foo.js
+++ examples/foo.js
@@ -12,17 +12,43 @@
 var sys = require('sys'),
     fs = require('fs');
 
+/**
+ * Files used by exports.output();
+ */
+
 var files = ['foo.js', 'bar.js'];
 
-for (var i = 0, len = files.length; i < len; ++i) {
-    var file = files[i];
-    if (file.match(/\.js$/)) {
-        fs.readFile(file, function(err, str){
-           if (err) {
-               throw err;
-           } else {
-               sys.puts(str); 
-           }
-        });
+/**
+ * Output contents of 'foo.js' and 'bar.js'
+ *
+ * ## WTF:
+ *
+ * This is just a stupid random demo, so... here is a list:
+ *
+ *   - foo.js
+ *   - bar.js
+ *
+ * ## Examples:
+ *
+ *    require('foo').output();
+ *
+ * @param {String} nothing a string that does nothing
+ * @return {Object} exports for chaining
+ * @api public
+ */
+
+var output = exports.output = function(nothing){
+    for (var i = 0, len = files.length; i < len; ++i) {
+        var file = files[i];
+        if (file.match(/\.js$/)) {
+            fs.readFile(file, function(err, str){
+               if (err) {
+                   throw err;
+               } else {
+                   sys.puts(str); 
+               }
+            });
+        }
     }
+    return this;
 }
