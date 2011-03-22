c1a579c00bdf840325fef4108811d4a906587111
Tj Holowaychuk <tj@vision-media.ca>
2011-03-22 13:43:31 -0700

    misc

--- templates/default/index.js
+++ templates/default/index.js
@@ -7,22 +7,28 @@ var jade = require('jade')
   , fs = require('fs');
 
 module.exports = function(env){
+  renderPages(env, function(err){
+    if (err) throw err;
+  });
+};
+
+function renderPages(env, fn) {
   var pending = env.paths.length;
 
   // render pages
   env.paths.forEach(function(path){
     var comments = env.files[path];
     page({ comments: comments, filename: path }, function(err, html){
-      if (err) throw err;
+      if (err) return fn(err);
       comments.html = html;
       layout({ body: html }, function(err, html){
-        if (err) throw err;
+        if (err) return fn(err);
         env.log('render', path);
-        --pending || done();
+        --pending || fn();
       });
     });
   });
-};
+}
 
 function page(locals, fn) {
   var options = { cache: true, locals: locals };
