24bedc69759b012b82210ca0df7084fa9909241f
Tj Holowaychuk <tj@vision-media.ca>
2011-03-22 13:40:46 -0700

    rendering layout

--- templates/default/index.js
+++ templates/default/index.js
@@ -1,4 +1,35 @@
 
+/**
+ * Module dependencies.
+ */
+
+var jade = require('jade')
+  , fs = require('fs');
+
 module.exports = function(env){
-  console.log(env);
+  var pending = env.paths.length;
+
+  // render pages
+  env.paths.forEach(function(path){
+    var comments = env.files[path];
+    page({ comments: comments, filename: path }, function(err, html){
+      if (err) throw err;
+      comments.html = html;
+      layout({ body: html }, function(err, html){
+        if (err) throw err;
+        env.log('render', path);
+        --pending || done();
+      });
+    });
+  });
 };
+
+function page(locals, fn) {
+  var options = { cache: true, locals: locals };
+  jade.renderFile(__dirname + '/page.jade', options, fn);
+}
+
+function layout(locals, fn) {
+  var options = { cache: true, locals: locals };
+  jade.renderFile(__dirname + '/layout.jade', options, fn);
+}
