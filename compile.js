e04d2703e0d969a1227bc6401928ab3ef5359dc1
Tj Holowaychuk <tj@vision-media.ca>
2011-10-08 15:29:38 -0700

    highlight strings

--- compile.js
+++ compile.js
@@ -7,6 +7,11 @@
 var jade = require('jade')
   , fs = require('fs');
 
+function highlight(js) {
+  return js
+    .replace(/('[^']*')/g, "<span class='string'>$1</span>");
+}
+
 var buf = '';
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(chunk){ buf += chunk; });
@@ -14,6 +19,9 @@ process.stdin.on('end', function(){
   var obj = JSON.parse(buf);
   fs.readFile('page.jade', 'utf8', function(err, str){
     if (err) throw err;
-    process.stdout.write(jade.render(str, { locals: { comments: obj }}));
+    var locals = {};
+    locals.comments = obj;
+    locals.highlight = highlight;
+    process.stdout.write(jade.render(str, { locals: locals }));
   });
 }).resume();
