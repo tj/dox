afcc98348943df41f125a164c2a0460d8ccc3623
Stephen Mathieson <smath23@gmail.com>
2013-03-13 19:00:05 -0400

    prototype properties

---
+++ test/fixtures/prototypes.js
@@ -0,0 +1,23 @@
+
+/**
+ * Does a lot of foo
+ *
+ * @param {String} bar
+ */
+function Foo(bar) {
+  this.bar = bar
+}
+
+/**
+ * A property of an instance of Foo
+ * @type {String}
+ */
+Foo.prototype.property = 'this is a property'
+
+/**
+ * A method of an instance of Foo
+ * @return {Boolean}
+ */
+Foo.prototype.method = function () {
+  return false
+}
