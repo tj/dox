2636c2033e9baf7c08ba9d792b0c23570e54315c
Dongsu Jang <iolothebard@gmail.com>
2013-10-31 21:05:51 +0900

    multiline tags such as @example

---
+++ test/fixtures/multilinetags.js
@@ -0,0 +1,143 @@
+/**
+ * only
+ *
+ * @sample
+ * one
+ * two
+ * three
+ */
+function only() {
+}
+
+/**
+ * first
+ *
+ * @sample
+ * one
+ * two
+ * three
+ * @bar last
+ */
+function first() {
+}
+
+/**
+ * last
+ *
+ * @foo first
+ * @sample
+ * one
+ * two
+ * three
+ */
+function last() {
+}
+
+/**
+ * mid
+ *
+ * @foo first
+ * @sample
+ * one
+ * two
+ * three
+ * @bar last
+ */
+function mid() {
+}
+
+/**
+ * only
+ *
+ * @param {String} foo
+ * one
+ * two
+ * three
+ */
+function onlyParam() {
+}
+
+/**
+ * first
+ *
+ * @param {String} foo
+ * one
+ * two
+ * three
+ * @bar last
+ */
+function firstParam() {
+}
+
+/**
+ * last
+ *
+ * @foo first
+ * @param {String} foo
+ * one
+ * two
+ * three
+ */
+function lastParam() {
+}
+
+/**
+ * mid
+ *
+ * @foo first
+ * @param {String} foo
+ * one
+ * two
+ * three
+ * @bar last
+ */
+function midParam() {
+}
+
+/**
+ * only
+ *
+ * @return {String}
+ * one
+ * two
+ * three
+ */
+function onlyReturn() {
+}
+
+/**
+ * first
+ *
+ * @return {String}
+ * one
+ * two
+ * three
+ * @bar last
+ */
+function firstReturn() {
+}
+
+/**
+ * last
+ *
+ * @foo first
+ * @return {String}
+ * one
+ * two
+ * three
+ */
+function lastReturn() {
+}
+
+/**
+ * mid
+ *
+ * @foo first
+ * @return {String}
+ * one
+ * two
+ * three
+ * @bar last
+ */
+function midReturn() {
+}
