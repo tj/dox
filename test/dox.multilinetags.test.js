a9e65ebb2299c3991e5a106bd4ee3a74320c071a
Mike Pennisi <mike@mikepennisi.com>
2014-12-25 00:58:20 -0500

    Parse tag strings with Markdown

--- test/dox.multilinetags.test.js
+++ test/dox.multilinetags.test.js
@@ -29,17 +29,17 @@ module.exports = {
         , example = comments.shift();
 
       only.tags.should.with.lengthOf(1);
-      only.tags[0].string.should.equal('one\ntwo\nthree');
+      only.tags[0].string.should.equal('<p>one<br />two<br />three</p>');
       first.tags.should.with.lengthOf(2);
-      first.tags[0].string.should.equal('one\ntwo\nthree');
-      first.tags[1].string.should.equal('last');
+      first.tags[0].string.should.equal('<p>one<br />two<br />three</p>');
+      first.tags[1].string.should.equal('<p>last</p>');
       last.tags.should.with.lengthOf(2);
-      last.tags[0].string.should.equal('first');
-      last.tags[1].string.should.equal('one\ntwo\nthree');
+      last.tags[0].string.should.equal('<p>first</p>');
+      last.tags[1].string.should.equal('<p>one<br />two<br />three</p>');
       mid.tags.should.with.lengthOf(3);
-      mid.tags[0].string.should.equal('first');
-      mid.tags[1].string.should.equal('one\ntwo\nthree');
-      mid.tags[2].string.should.equal('last');
+      mid.tags[0].string.should.equal('<p>first</p>');
+      mid.tags[1].string.should.equal('<p>one<br />two<br />three</p>');
+      mid.tags[2].string.should.equal('<p>last</p>');
 
       onlyParam.tags.should.with.lengthOf(1);
       onlyParam.tags[0].type.should.equal('param');
@@ -51,20 +51,20 @@ module.exports = {
       firstParam.tags[0].name.should.equal('foo');
       firstParam.tags[0].types.should.eql(['String']);
       firstParam.tags[0].description.should.equal('<p>one<br />two<br />three</p>');
-      firstParam.tags[1].string.should.equal('last');
+      firstParam.tags[1].string.should.equal('<p>last</p>');
       lastParam.tags.should.with.lengthOf(2);
-      lastParam.tags[0].string.should.equal('first');
+      lastParam.tags[0].string.should.equal('<p>first</p>');
       lastParam.tags[1].type.should.equal('param');
       lastParam.tags[1].name.should.equal('foo');
       lastParam.tags[1].types.should.eql(['String']);
       lastParam.tags[1].description.should.equal('<p>one<br />two<br />three</p>');
       midParam.tags.should.with.lengthOf(3);
-      midParam.tags[0].string.should.equal('first');
+      midParam.tags[0].string.should.equal('<p>first</p>');
       midParam.tags[1].type.should.equal('param');
       midParam.tags[1].name.should.equal('foo');
       midParam.tags[1].types.should.eql(['String']);
       midParam.tags[1].description.should.equal('<p>one<br />two<br />three</p>');
-      midParam.tags[2].string.should.equal('last');
+      midParam.tags[2].string.should.equal('<p>last</p>');
 
       onlyReturn.tags.should.with.lengthOf(1);
       onlyReturn.tags[0].type.should.equal('return');
@@ -74,21 +74,21 @@ module.exports = {
       firstReturn.tags[0].type.should.equal('return');
       firstReturn.tags[0].types.should.eql(['String']);
       firstReturn.tags[0].description.should.equal('<p>one<br />two<br />three</p>');
-      firstReturn.tags[1].string.should.equal('last');
+      firstReturn.tags[1].string.should.equal('<p>last</p>');
       lastReturn.tags.should.with.lengthOf(2);
-      lastReturn.tags[0].string.should.equal('first');
+      lastReturn.tags[0].string.should.equal('<p>first</p>');
       lastReturn.tags[1].type.should.equal('return');
       lastReturn.tags[1].types.should.eql(['String']);
       lastReturn.tags[1].description.should.equal('<p>one<br />two<br />three</p>');
       midReturn.tags.should.with.lengthOf(3);
-      midReturn.tags[0].string.should.equal('first');
+      midReturn.tags[0].string.should.equal('<p>first</p>');
       midReturn.tags[1].type.should.equal('return');
       midReturn.tags[1].types.should.eql(['String']);
       midReturn.tags[1].description.should.equal('<p>one<br />two<br />three</p>');
-      midReturn.tags[2].string.should.equal('last');
+      midReturn.tags[2].string.should.equal('<p>last</p>');
 
       example.tags.should.with.lengthOf(1);
-      example.tags[0].string.should.equal('    test(one);\n    test(two);');
+      example.tags[0].string.should.equal('<pre><code>test(one);\ntest(two);\n</code></pre>');
       done();
     });
   }
--- test/dox.test.js
+++ test/dox.test.js
@@ -140,12 +140,13 @@ module.exports = {
       parseTagTypes.codeStart.should.equal(172);
 
       var escape = comments.pop();
-      escape.tags.should.have.length(3);
+      escape.tags.should.have.length(4);
+      escape.tags[3].string.should.equal('<p>With <code>Markdown</code> syntax</p>')
       escape.description.full.should.equal('<p>Escape the given <code>html</code>.</p>');
       escape.ctx.type.should.equal('function');
       escape.ctx.name.should.equal('escape');
       escape.line.should.equal(253);
-      escape.codeStart.should.equal(261);
+      escape.codeStart.should.equal(262);
       done();
     });
   },
