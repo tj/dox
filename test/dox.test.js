ad55d5b67dd8c4f8b9b850699e2043506a923396
Jay Oster <jay@kodewerx.org>
2015-04-19 13:53:15 -0700

    Ensure typeless @enum is given expected tag properties.

--- test/dox.test.js
+++ test/dox.test.js
@@ -896,10 +896,12 @@ module.exports = {
       comments.length.should.equal(2);
       comments[0].description.full.should.equal("<p>FSM states.</p>");
       comments[0].tags[0].type.should.equal("enum");
+      comments[0].tags[0].types.length.should.equal(0);
       comments[0].tags[0].string.should.equal("");
 
       comments[1].description.full.should.equal("<p>Colors.</p>");
       comments[1].tags[0].type.should.equal("enum");
+      comments[0].tags[0].types.length.should.equal(0);
       comments[1].tags[0].string.should.equal("");
       done();
     });
