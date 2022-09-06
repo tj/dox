08a8cc76e84199c55f8dedb0155a7ec0f6b3d798
Scott Doxey <neogeek0101@gmail.com>
2022-09-06 00:26:20 -0400

    Added generic type test.

---
+++ test/fixtures/types.js
@@ -0,0 +1,7 @@
+/**
+ * @return {Promise<string | null>} Promise that returns a string if available.
+ */
+
+/**
+ * @return {Promise.<string | null>} Promise that returns a string if available.
+ */
